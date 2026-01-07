a0c21fe2eed15542b5d9f8765ecf35b590604af2
Arsen <64782042+DevArsen@users.noreply.github.com>
2026-01-08 00:17:33 +0200

    Add Ball class implementation for Pong game
    Introduces Ball.h and Ball.cpp, defining the Ball class with position, shape, movement logic, and rebound methods for use in a Pong game. Includes constructor, getters, and rebound handling for sides, bat/top, and bottom.

---
+++ Pong/Ball.cpp
@@ -0,0 +1,37 @@
+﻿#include "Ball.h"
+
+// Функція-конструктор
+Ball::Ball(float startX, float startY) : m_Position(startX, startY)
+{
+	m_Shape.setSize(sf::Vector2f(10, 10)); // Встановлюємо розмір м'яча
+	m_Shape.setPosition(m_Position); // Встановлюємо початкову позицію м'яча
+
+}
+
+FloatRect Ball::getPosition()
+{
+	return m_Shape.getGlobalBounds(); // Повертаємо глобальні межі форми м'яча
+}
+RectangleShape Ball::getShape()
+{
+	return m_Shape; // Повертаємо об'єкт форми м'яча
+}
+float Ball::getXVelocity()
+{
+	return m_DirectionX;
+}
+void Ball::reboundSides()
+{
+	m_DirectionX = -m_DirectionX; // Змінюємо напрямок руху по осі X
+}
+void Ball::reboundBatOrTop()
+{
+	m_DirectionY = -m_DirectionY; // Змінюємо напрямок руху по осі Y
+}
+void Ball::reboundBottom()
+{
+	m_Position.y = 0; // Скидаємо позицію м'яча по осі Y
+	m_Position.x = 500; // Скидаємо позицію м'яча по осі X
+	m_DirectionY = -m_DirectionY; // Змінюємо напрямок руху по осі Y
+}
+//204  
---
+++ Pong/Ball.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+using namespace sf; 
+class Ball
+{
+private:
+	Vector2f m_Position; // Позиція м'яча
+	RectangleShape m_Shape; // Об'єкт RectangleShape для м'яча
+	float m_Speed = 300.0f; // Швидкість м'яча
+	float m_DirectionX = .2f; // Напрямок руху по осі X
+	float m_DirectionY = .2f; // Напрямок руху по осі Y
+public:
+	Ball(float startX, float startY); // Конструктор м'яча
+	FloatRect getPosition(); // Отримати позицію м'яча
+	RectangleShape getShape(); // Отримати форму м'яча
+	void reboundSides(); // Відскок від сторін
+	void reboundBatOrTop(); // Відскок від ракетки або верху
+	void reboundBottom(); // Відскок від низу
+	void update(Time dt); // Оновлення позиції м'яча
+};
